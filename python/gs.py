a3966010548239cb567d749b0516f069117112fa
Nicolas Martins <nimartins.na1@gmail.com>
2023-11-28 09:21:32 -0300

    Criado a função de enviar exames

--- python/gs.py
+++ python/gs.py
@@ -6,3 +6,10 @@ def criar_paciente(nome, email):
         print(f'Paciente {nome} criado com sucesso.')
     else:
         print(f'O Paciente {nome} já existe.')
+
+def enviar_exame(nome_paciente, dados_exame):
+    if nome_paciente in pacientes:
+        pacientes[nome_paciente]['exames'].append(dados_exame)
+        print(f'Exame enviado para {nome_paciente} com sucesso.')
+    else:
+        print(f'O paciente {nome_paciente} não encontrado.')
